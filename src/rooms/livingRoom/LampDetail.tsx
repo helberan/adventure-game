292f2e0867f750fa5b4c9ee27e63d5855b0d04e5
Helena Beranová <helca.beranova@gmail.com>
2024-08-14 15:24:28 +0200

    build

--- src/rooms/livingRoom/LampDetail.tsx
+++ src/rooms/livingRoom/LampDetail.tsx
@@ -1,17 +1,14 @@
 import { useNavigate } from 'react-router';
-import { useState } from 'react';
 import LampDetailImage from '../../assets/lamp-detail.png';
 import keyImage from '../../assets/key.png';
 import '../routing.css';
 
 export const LampDetail = () => {
   const navigate = useNavigate();
-  const [keyPickedUp, setKeyPickedUp] = useState<boolean>(false);
 
   const handleItemClick = (item: string) => {
     console.log(`clicked ${item}`);
     if (item === 'key') {
-      setKeyPickedUp(true);
       localStorage.setItem('keyPickedUp', 'true');
     }
   };
